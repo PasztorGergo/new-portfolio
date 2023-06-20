a415f5c1e27e2bd4cbdc9950055feac71f0ad8f3
PasztorGergo <pasztorg05@gmail.com>
2023-06-20 17:02:48 +0200

    responsive-navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,21 +2,22 @@
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { useMediaQuery, useWindowScroll } from "@mantine/hooks";
-import { useAnimation } from "framer-motion";
+import { RiCloseFill, RiMenu3Fill } from "react-icons/ri";
+import { AnimatePresence, motion } from "framer-motion";
+import { fadeIn, staggerContainer } from "utils";
 
 export default function Navbar() {
   const [open, setOpen] = useState<boolean>(false);
-  const [scrollPos, scrollTo] = useWindowScroll();
+  const scrollPos = window.scrollY;
 
   return (
     <nav
       className={`w-full bg-[#1c1d22] py-4 px-16 flex flex-row justify-between items-center ${
-        scrollPos.y > 30 ? "fixed" : "absolute"
+        scrollPos > 30 ? "fixed" : "absolute"
       } z-30 top-0 left-0`}
     >
       <Image alt="Logo" src="/favicon.svg" width={50.41} height={50.25} />
-      <ul className="flex justify-between items-center grow-[0.25]">
+      <ul className="hidden md:flex justify-between items-center grow-[0.25]">
         <li
           onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
           className="uppercase tracking-wide text-center font-bold hover:border-b-2 border-b-brand border-0 opacity-75 hover:opacity-100 cursor-pointer transition-all duration-100 text-sm"
@@ -45,6 +46,87 @@ export default function Navbar() {
           Testimonails
         </li>
       </ul>
+      <RiMenu3Fill
+        className="text-[32px] text-white md:hidden inline"
+        onClick={() => setOpen(true)}
+      />
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ x: "-30%", opacity: 0, filter: "blur(1rem)" }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              filter: "blur(0px)",
+              transition: { type: "tween", duration: 0.5 },
+            }}
+            exit={{
+              x: "30%",
+              opacity: 0,
+              filter: "blur(1rem)",
+              transition: { type: "tween", duration: 0.5 },
+            }}
+            className="top-0 left-0 fixed w-full h-screen grid place-items-center"
+          >
+            <motion.ul
+              variants={staggerContainer}
+              initial="hidden"
+              whileInView="show"
+            >
+              <motion.li
+                variants={fadeIn("right", 12, 60)}
+                className="uppercase tracking-wide text-center font-bold hover:border-b-2 border-b-brand border-0 opacity-75 hover:opacity-100 cursor-pointer transition-all duration-100 text-sm"
+                onClick={() => {
+                  setOpen(false);
+                  window.scrollTo({ top: 0, behavior: "smooth" });
+                }}
+              >
+                Home
+              </motion.li>
+              <motion.li
+                variants={fadeIn("right", 12, 60)}
+                className="uppercase tracking-wide text-center font-bold hover:border-b-2 border-b-brand border-0 opacity-75 hover:opacity-100 cursor-pointer transition-all duration-100 text-sm"
+                onClick={() => {
+                  setOpen(false);
+                  window.scrollTo({ top: 600, behavior: "smooth" });
+                }}
+              >
+                About
+              </motion.li>
+              <motion.li
+                variants={fadeIn("right", 12, 60)}
+                className="uppercase tracking-wide text-center font-bold hover:border-b-2 border-b-brand border-0 opacity-75 hover:opacity-100 cursor-pointer transition-all duration-100 text-sm"
+                onClick={() => {
+                  setOpen(false);
+                  window.scrollTo({ top: 1200, behavior: "smooth" });
+                }}
+              >
+                Skills
+              </motion.li>
+              <motion.li
+                variants={fadeIn("right", 12, 60)}
+                className="uppercase tracking-wide text-center font-bold hover:border-b-2 border-b-brand border-0 opacity-75 hover:opacity-100 cursor-pointer transition-all duration-100 text-sm"
+                onClick={() => {
+                  setOpen(false);
+                  window.scrollTo({ top: 1750, behavior: "smooth" });
+                }}
+              >
+                Projects
+              </motion.li>
+              <motion.li
+                variants={fadeIn("right", 12, 60)}
+                className="uppercase tracking-wide text-center font-bold hover:border-b-2 border-b-brand border-0 opacity-75 hover:opacity-100 cursor-pointer transition-all duration-100 text-sm"
+                onClick={() => {
+                  setOpen(false);
+                  window.scrollTo({ top: 2300, behavior: "smooth" });
+                }}
+              >
+                Testimonials
+              </motion.li>
+            </motion.ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 }
